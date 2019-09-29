9d379e474b2a456fc934483f0d17318156a2a188
Edward89crypto <stasakedward@gmail.com>
2019-09-30 01:30:49 +0300

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
-
+import './server.js'
 class App extends Component {
   constructor() {
     super();
